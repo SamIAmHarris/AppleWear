11679fdcf35e6e93190be1035fca34d5aa4f74af
Sam Harris <samh@jackrabbitmobile.com>
2015-12-23 10:21:11 -0600

    Add drag functionality to gray drag view. Change transparency of red/purple views if the gray drag view is inside them.

--- app/src/main/java/com/jackrabbitmobile/applewear/MainActivity.java
+++ app/src/main/java/com/jackrabbitmobile/applewear/MainActivity.java
@@ -1,54 +1,113 @@
 package com.jackrabbitmobile.applewear;
 
+import android.content.ClipData;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
 import android.support.wearable.view.BoxInsetLayout;
+import android.util.Log;
+import android.view.DragEvent;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.TextView;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
 
 public class MainActivity extends WearableActivity {
 
-    private static final SimpleDateFormat AMBIENT_DATE_FORMAT =
-            new SimpleDateFormat("HH:mm", Locale.US);
-
-    private BoxInsetLayout mContainerView;
+    View redView;
+    View grayView;
+    View purpleView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setAmbientEnabled();
+        
+        redView = findViewById(R.id.red_view);
+        purpleView = findViewById(R.id.purple_view);
+        grayView = findViewById(R.id.gray_view);
+        grayView.setOnTouchListener(new AWDragTouchListener());
 
-        mContainerView = (BoxInsetLayout) findViewById(R.id.container);
+        purpleView.setOnDragListener(new View.OnDragListener() {
+            @Override
+            public boolean onDrag(View v, DragEvent event) {
+                switch (event.getAction()) {
+                    case DragEvent.ACTION_DRAG_STARTED:
+                        Log.i("Drag", "Drag started");
+                        break;
+                    case DragEvent.ACTION_DRAG_ENTERED:
+                        purpleView.setBackground(getResources().getDrawable(R.drawable.transparent_purple_circle));
+                        Log.i("Drag", "Drag entered");
+                        break;
+                    case DragEvent.ACTION_DRAG_EXITED:
+                        purpleView.setBackground(getResources().getDrawable(R.drawable.purple_circle_view));
+                        Log.i("Drag", "Drag exited");
+                        break;
+                    case DragEvent.ACTION_DROP:
+                        Log.i("Drag", "Drag drop");
+                        break;
+                    case DragEvent.ACTION_DRAG_ENDED:
+                        Log.i("Drag", "Drag ended");
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });
+
+        redView.setOnDragListener(new View.OnDragListener() {
+            @Override
+            public boolean onDrag(View v, DragEvent event) {
+                switch (event.getAction()) {
+                    case DragEvent.ACTION_DRAG_STARTED:
+                        Log.i("Drag", "Drag started");
+                        break;
+                    case DragEvent.ACTION_DRAG_ENTERED:
+                        redView.setBackground(getResources().getDrawable(R.drawable.transparent_red_circle));
+                        Log.i("Drag", "Drag entered");
+                        break;
+                    case DragEvent.ACTION_DRAG_EXITED:
+                        redView.setBackground(getResources().getDrawable(R.drawable.red_circle_view));
+                        Log.i("Drag", "Drag exited");
+                        break;
+                    case DragEvent.ACTION_DROP:
+                        Log.i("Drag", "Drag drop");
+                        break;
+                    case DragEvent.ACTION_DRAG_ENDED:
+                        Log.i("Drag", "Drag ended");
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });
+
     }
 
     @Override
     public void onEnterAmbient(Bundle ambientDetails) {
         super.onEnterAmbient(ambientDetails);
-        //updateDisplay();
     }
 
     @Override
     public void onUpdateAmbient() {
         super.onUpdateAmbient();
-        //updateDisplay();
     }
 
     @Override
     public void onExitAmbient() {
-        //updateDisplay();
         super.onExitAmbient();
     }
 
-    private void updateDisplay() {
-        if (isAmbient()) {
-            mContainerView.setBackgroundColor(getResources().getColor(android.R.color.black));
-        } else {
-            mContainerView.setBackground(null);
+    private final class AWDragTouchListener implements View.OnTouchListener {
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                ClipData data = ClipData.newPlainText("", "");
+                View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+                view.startDrag(data, shadowBuilder, view, 0);
+                view.setVisibility(View.INVISIBLE);
+                return true;
+            } else {
+                return false;
+            }
         }
     }
 }
